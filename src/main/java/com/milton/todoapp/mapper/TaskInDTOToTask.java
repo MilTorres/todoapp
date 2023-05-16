265e73267cc9a1ed73f9259d3957e1fdc709851f
Milton Torres <Miltontorres018@gmail.com>
2023-05-16 11:11:30 -0600

    se termino de configurar la clase TaskInDTOToTask donde se hace el mapper

--- src/main/java/com/milton/todoapp/mapper/TaskInDTOToTask.java
+++ src/main/java/com/milton/todoapp/mapper/TaskInDTOToTask.java
@@ -1,14 +1,26 @@
 package com.milton.todoapp.mapper;
 
 import com.milton.todoapp.persistence.entity.Task;
+import com.milton.todoapp.persistence.entity.TaskStatus;
 import com.milton.todoapp.service.dto.TaskInDTO;
 import org.springframework.stereotype.Component;
 
-//Esta clase convierte un taskindto en un Task
+import java.time.LocalDateTime;
+
+//Esta clase convierte un taskindto en un Task crea un  elemento de entrada a un elemento de salida
+//El usuario solo agrega 3 datos titulo,descripcion y fechaestimada el mapper crea los dos objetos
+//crea la tarea del usuario con 3 datos y crea la tarea en general con los demas datos
 @Component
 public class TaskInDTOToTask implements IMapper<TaskInDTO, Task>{
     @Override
     public Task map(TaskInDTO in) {
+        Task task = new Task();
+        task.setTitulo(in.getTitulo());
+        task.setDescripcion(in.getDescripcion());
+        task.setFechaestimada(in.getFechaestimada());
+        task.setFechacreacion(LocalDateTime.now());
+        task.setFinalizada(false);
+        task.setTaskStatus(TaskStatus.ON_TIME);
         return null;
     }
 }
--- src/main/java/com/milton/todoapp/service/dto/TaskInDTO.java
+++ src/main/java/com/milton/todoapp/service/dto/TaskInDTO.java
@@ -1,9 +1,11 @@
 package com.milton.todoapp.service.dto;
 
 import com.milton.todoapp.persistence.entity.TaskStatus;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class TaskInDTO {
     private String titulo;
     private String descripcion;
