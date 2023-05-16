50bf09a9774c7f7a2e352fb61d9597d762dece4d
Milton Torres <Miltontorres018@gmail.com>
2023-05-16 10:47:30 -0600

    se crea la clase Task que contiene la creacion de la tabla task en H2 y los valores para las columnas

---
+++ src/main/java/com/milton/todoapp/persistence/entity/Task.java
@@ -0,0 +1,23 @@
+package com.milton.todoapp.persistence.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Data // crea la clase de lommbo que habilita los get and set sin nececidad de escribirlos
+@Entity //hacemos referencia a que esta clase es una entidad de base de datos
+@Table(name = "task") // crea la tabla en el SGBD H2 llamada task
+public class Task {
+    @Id // con esta notacion marcamos que tendremos una campo llamada id
+    @GeneratedValue(strategy = GenerationType.AUTO) // crea el id auto incrementable
+
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private LocalDateTime fechacreacion;
+    private LocalDateTime fechaterminada;
+    private boolean finalizada;
+    private TaskStatus taskStatus;
+}
---
+++ src/main/java/com/milton/todoapp/persistence/entity/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.milton.todoapp.persistence.entity;
+
+public enum TaskStatus {
+    ON_TIME,LATE
+}
